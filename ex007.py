33de1a62ec7a8483cdce902764ca8ba45975b4f7
PeduRibeiro <opeduu@gmail.com>
2026-04-27 14:30:07 -0300

    Fix input prompts and calculation for average

--- ex007.py
+++ ex007.py
@@ -1,6 +1,4 @@
 n1 = float(input('Qual a primeira nota?'))
 n2 = float(input('Qual a sgeunda nota?'))
-
 m = (n1 + n2) / 2
-
 print('Sendo as notas do aluno {} e {} sabesse que a media final dele é {}' .format(n1, n2, m))
