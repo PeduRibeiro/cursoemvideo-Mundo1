23ef3f6fb6efa8f5d3c30365e2528789be75eccc
PeduRibeiro <opeduu@gmail.com>
2026-04-27 14:31:13 -0300

    Update ex012.py

--- ex012.py
+++ ex012.py
@@ -1,4 +1,3 @@
 preco1 = float(input('Qual o valor do produto? R$ '))
 preco2 = preco1 * 0.95
 print('Sabendo que o valor do produto é R$ {} com o desconto de 5% será pago R$ {}'.format(preco1, preco2))
-
